86485ab3b83f4d20d71527a60d30edf5644ce44b
alexday <daiyunstar@gmail.com>
2014-06-17 12:04:41 +0800

    Initial Commit

---
+++ NTBELabs/SMHC_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMHC_AppDelegate.h
+//  NTBELabs
+//
+//  Created by dai yun on 14-6-17.
+//  Copyright (c) 2014年 alexday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMHC_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NTBELabs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NTBELabs
+//
+//  Created by dai yun on 14-6-17.
+//  Copyright (c) 2014年 alexday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMHC_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMHC_AppDelegate class]));
+    }
+}
